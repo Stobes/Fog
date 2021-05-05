9949818628e556950304cc1461a286fb418abfe7
Buchandreas <43332738+Buchandreas@users.noreply.github.com>
2021-05-05 16:00:21 +0200

    Update project war

---
+++ src/main/java/business/persistence/fixer.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE  IF NOT EXISTS FOG_Carport_test;
+
+USE startcode;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  email varchar(90) NOT NULL,
+  password varchar(45) NOT NULL,
+  role varchar(20) NOT NULL DEFAULT 'customer',
+  PRIMARY KEY (id),
+  UNIQUE KEY email_UNIQUE (email)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+LOCK TABLES users WRITE;
+INSERT INTO users VALUES 
+(1,'barbie@world.dk','jensen','customer'),
+(2,'ken@world.com','jensen','customer'),
+(3,'robin@gotham.com','batman','employee');
+UNLOCK TABLES;
