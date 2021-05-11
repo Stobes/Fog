e817116f3ce9ce20652b1b258bbdf2fae408cba9
SebastianEngelbrecht <71072482+SebastianEngelbrecht@users.noreply.github.com>
2021-05-11 11:46:04 +0200

    Værdier indsat i sql

---
+++ src/main/java/business/persistence/Material.sql
@@ -0,0 +1,25 @@
+SELECT * FROM fog_carport.material;
+
+insert into fog_carport.material (id,unit,material_description, meter_price)
+values
+(1,'stk','25x200 mm. trykimp. Brædt',1),
+(2,'stk','25x125mm. trykimp. Brædt',1),
+(3,'stk','38x73 mm. Lægte ubh.',1),
+(4,'stk','45x95 mm. Reglar ub.',1),
+(5,'stk','45x195 mm. spærtræ ubh',1),
+(6,'stk','97x97 mm. trykimp. Stolpe',1),
+(7,'stk','19x100 mm. trykimp. Brædt',1),
+(8,'stk','Plastmo Ecolite blåtonet',1),
+(9,'pakke','plastmo bundskruer 200 stk.',1),
+(10,'rulle','hulbånd 1x20 mm. 10 mtr.',1),
+(11,'stk','universal 190 mm højre',1),
+(12,'stk','universal 190 mm venstre',1),
+(13,'pakke','4,5 x 60 mm. skruer 200 stk.',1),
+(14,'pakke','4,0 x 50 mm. beslagskruer 250 stk.',1),
+(15,'stk','bræddebolt 10 x 120 mm.',1),
+(16,'stk','firkantskiver 40x40x11mm',1),
+(17,'pakke','4,5 x 70 mm. Skruer 400 stk.',1),
+(18,'pakke','4,5 x 50 mm. Skruer 300 stk.',1),
+(19,'sæt','stalddørsgreb 50x75',1),
+(20,'stk','t hængsel 390 mm',1),
+(21,'stk','vinkelbeslag 35',1);
